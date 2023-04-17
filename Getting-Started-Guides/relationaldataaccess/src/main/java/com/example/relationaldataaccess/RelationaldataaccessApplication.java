cc28020cc51f5d42edf02f1a4c83a0882466c87c
starryeye <see2818@naver.com>
2023-04-17 21:03:17 +0900

    [starryeye, 2023.04.17] - 4. Accessing Relational Data using JDBC with Spring
    내용
    - init
    - https://spring.io/guides/gs/relational-data-access/

---
+++ Getting-Started-Guides/relationaldataaccess/src/main/java/com/example/relationaldataaccess/RelationaldataaccessApplication.java
@@ -0,0 +1,13 @@
+package com.example.relationaldataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationaldataaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationaldataaccessApplication.class, args);
+	}
+
+}
