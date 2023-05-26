567708c1d119906e79494bdc42c512059bd4fa7a
starryeye <see2818@naver.com>
2023-05-26 21:14:56 +0900

    [starryeye, 2023.05.26] - 6. Testing the Web Layer
    내용
    - init
    - https://spring.io/guides/gs/testing-web/

---
+++ Getting-Started-Guides/TestWeb/src/test/java/dev/practice/TestWeb/TestWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.TestWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
