b36e2d8e3a8815f3d3821efcd17fd8f648d38312
starryeye <see2818@naver.com>
2023-05-26 22:33:00 +0900

    [starryeye, 2023.05.26] - 6. Testing the Web Layer
    내용
    - @WebMvcTest 를 이용한 Test
    - https://spring.io/guides/gs/testing-web/

--- Getting-Started-Guides/TestWeb/src/test/java/dev/practice/TestWeb/TestWebApplicationTest.java
+++ Getting-Started-Guides/TestWeb/src/test/java/dev/practice/TestWeb/TestWebApplicationTest.java
@@ -14,6 +14,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
  * @SpringBootTest 로 Spring Boot Application 전체 컨텍스트가 띄워진다.
  * @AutoConfigureMockMvc 로 띄워진 컨텍스트내에 MockMvc 인스턴스를 생성하고 웹 계층을 모의 테스트 할 수 있도록 돕는다.
  * 즉, HTTP 요청과 응답을 모방하여 실제 서버를 띄우지 않고도 웹 계층을 테스트하는 것이 가능하다.
+ *
+ * @SpringBootTest + @AutoConfiguration 조합은
+ * @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) 보다 로딩이 단순하여
+ * Test 수행 속도가 빠르다.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
---
+++ Getting-Started-Guides/TestWeb/src/test/java/dev/practice/TestWeb/WebLayerTest.java
@@ -0,0 +1,41 @@
+package dev.practice.TestWeb;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+/**
+ * @WebMvcTest 는 Spring MVC Controller 에 집중된 Test 방법이다.
+ * - @Controller, @ControllerAdvice, @JsonComponent,
+ * Filter, WebMvcConfigurer, HandlerMethodArgumentResolver 등의 Bean 만 로드한다.
+ * - MockMvc Bean 을 생성하여 로드한다.
+ * @SpringBootTest 가 없어서 Spring Boot Application 전체 컨텍스트가 띄워지지 않으므로, 스프링 빈이 주입될 수 없다.
+ * 그래서, @Service, @Component 등의 빈을 이용하기 위해서는 Mock 객체를 이용해야 한다.
+ *
+ * @WebMvcTest 는 @SpringBootTest + @AutoConfigurationMockMvc 조합 보다 로딩이 단순하여 Test 수행 속도가 빠르다.
+ */
+@WebMvcTest(controllers = HomeController.class)
+public class WebLayerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * TestWebApplication 과 동일
+     */
+    @Test
+    void shouldReturnDefaultMessage() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(
+                        Matchers.containsString("Hello, World")
+                ));
+    }
+}
